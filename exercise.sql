7469d12dc16fa85efcb0c837ef4e162687e2fa3d
Joris ter Wolbeek <21074092@talnet.nl>
2020-02-13 13:04:38 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -7,4 +7,4 @@ UPDATE planeten
 SET Bezoek = "0001-01-01"
 WHERE Naam = "Aarde";
 INSERT INTO planeten (Naam, Diameter, AfstandTotZon, Massa, Bezoek)
-VALUES ("Maan", 17371, 384.401, 73500000000000000000000, 1961/07/20);
+VALUES ("Maan", 17371, 384.401, 73500000000000000000000, "1961-07-20");
